057b747fa923f33aed4ba0d98ed6cfd9d3e3afb7
SanzharShadybekov <shadubekovs777@gmail.com>
2022-10-07 12:35:13 +0600

    added viewset

--- blogApi/settings.py
+++ blogApi/settings.py
@@ -33,6 +33,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_auth',
+    'drf_yasg',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
--- main/views.py
+++ main/views.py
@@ -2,9 +2,21 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import generics, permissions
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.filters import SearchFilter
+from rest_framework.decorators import action
+from django_filters.rest_framework import DjangoFilterBackend
 from main.models import Category, Post, Comment
 from . import serializers
 from .permissions import IsAuthor
+
+
+
+class StandartResultPagination(PageNumberPagination):
+    page_size = 3
+    page_query_param = 'page'
+    max_page_size = 1000
 
 
 class CategoryListView(generics.ListCreateAPIView):
@@ -12,31 +24,42 @@ class CategoryListView(generics.ListCreateAPIView):
     serializer_class = serializers.CategorySerializer
 
 
-class PostListCreateView(generics.ListCreateAPIView):
+class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-
-    def get_serializer_class(self):
-        if self.request.method == 'GET':
-            return serializers.PostListSerializer
-        return serializers.PostCreateSerializer
+    pagination_class = StandartResultPagination
+    filter_backends = (SearchFilter, DjangoFilterBackend)
+    search_fields = ('title',)
+    filterset_fields = ('owner', 'category')
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
-
-class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Post.objects.all()
+    def get_serializer_class(self):
+        if self.action in ('create', 'update', 'partial_update'):
+            return serializers.PostCreateSerializer
+        elif self.action == 'retrieve':
+            return serializers.PostSerializer
+        else:
+            return serializers.PostListSerializer
 
     def get_permissions(self):
-        if self.request.method in ('PUT', 'PATCH', 'DELETE'):
+        # Изменять и удалять пост может только автор поста
+        if self.action in ('update', 'partial_update', 'destroy'):
             return [permissions.IsAuthenticated(), IsAuthor()]
-        return [permissions.AllowAny()]
+        # Создавать может залогиненный юзер
+        elif self.action == 'create':
+            return [permissions.IsAuthenticated()]
+        # просматривать могут все
+        else:
+            return [permissions.AllowAny()]
 
-    def get_serializer_class(self):
-        if self.request.method in ('PUT', 'PATCH'):
-            return serializers.PostCreateSerializer
-        return serializers.PostSerializer
+    # /posts/<id>/comments/
+    @action(['GET'], detail=True)
+    def comments(self, request, pk):
+        post = self.get_object()
+        comments = post.comments.all()
+        serializer = serializers.CommentSerializer(comments, many=True)
+        return Response(serializer.data, status=200)
 
 
 class CommentListCreateView(generics.ListCreateAPIView):
@@ -57,6 +80,32 @@ class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
             return [permissions.AllowAny()]
         return [permissions.IsAuthenticated(), IsAuthor()]
 
+
+# class PostListCreateView(generics.ListCreateAPIView):
+#     queryset = Post.objects.all()
+#     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+#
+#     def get_serializer_class(self):
+#         if self.request.method == 'GET':
+#             return serializers.PostListSerializer
+#         return serializers.PostCreateSerializer
+#
+#     def perform_create(self, serializer):
+#         serializer.save(owner=self.request.user)
+#
+#
+# class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Post.objects.all()
+#
+#     def get_permissions(self):
+#         if self.request.method in ('PUT', 'PATCH', 'DELETE'):
+#             return [permissions.IsAuthenticated(), IsAuthor()]
+#         return [permissions.AllowAny()]
+#
+#     def get_serializer_class(self):
+#         if self.request.method in ('PUT', 'PATCH'):
+#             return serializers.PostCreateSerializer
+#         return serializers.PostSerializer
 
 # function based view
 # @api_view(['GET'])
